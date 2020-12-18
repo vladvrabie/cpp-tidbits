a020cf499e3187765b2dc1883eb6fe346b1f1cc4
Vlad Vrabie <vladvrabie97@gmail.com>
2020-12-18 19:06:15 +0100

    Refactored timing methods

--- cpp20-timer/cpp20-timer/timing.h
+++ cpp20-timer/cpp20-timer/timing.h
@@ -26,27 +26,27 @@ template <class T>
 concept duration = is_duration_v<T>;
 
 
-template <duration D = std::chrono::nanoseconds>
+template <duration Duration = std::chrono::nanoseconds>
 class Timer
 {
 public:
 
-	template <duration D = std::chrono::nanoseconds>
+	template <duration Duration = std::chrono::nanoseconds>
 	struct Statistics
 	{
 		double mean;
 		double variance;
 		double standard_deviation;
-		std::vector<D> durations;
+		std::vector<Duration> durations;
 
-		explicit Statistics(std::vector<D>&& times)
+		explicit Statistics(std::vector<Duration>&& times)
 			: durations{ times }
 		{
 			double sum = std::accumulate(
 				durations.cbegin(),
 				durations.cend(),
 				0.0,
-				[]<duration D>(double&& sum, const D& time)
+				[]<duration Duration>(double&& sum, const Duration& time)
 				{
 					return sum + static_cast<double>(time.count());
 				}
@@ -57,7 +57,7 @@ public:
 				durations.cbegin(),
 				durations.cend(),
 				0.0,
-				[&mean = std::as_const(mean)]<duration D>(double&& sum, const D& time)
+				[&mean = std::as_const(mean)]<duration Duration>(double&& sum, const Duration& time)
 				{
 					return sum + std::pow(static_cast<double>(time.count()) - mean, 2);
 				}
@@ -67,7 +67,7 @@ public:
 			standard_deviation = std::sqrt(variance);
 		}
 
-		friend std::ostream& operator<< (std::ostream& out, const Statistics<D>& stats)
+		friend std::ostream& operator<< (std::ostream& out, const Statistics<Duration>& stats)
 		{
 			out << "Mean: " << stats.mean << '\n'
 				<< "Variance: " << stats.variance << '\n'
@@ -85,9 +85,9 @@ public:
 
 	template <class Function, class... Args>
 	requires std::invocable<Function, Args...>
-		&& std::is_assignable_v<std::invoke_result_t<Function, Args...>, std::invoke_result_t<Function, Args...>>
-	static
-	auto time_function(Function&& f, Args&&... args) -> D
+		  && std::is_assignable_v<std::invoke_result_t<Function, Args...>,
+							      std::invoke_result_t<Function, Args...>>
+	[[nodiscard]] static auto time_function(Function&& f, Args&&... args) -> Duration
 	{
 		auto start = std::chrono::high_resolution_clock::now();
 		if constexpr (std::is_same_v<std::invoke_result_t<Function, Args...>, void>)
@@ -96,69 +96,78 @@ public:
 		}
 		else
 		{
-			auto result = std::invoke(
-				std::forward<Function>(f),
-				std::forward<Args>(args)...
-			);
+			auto result = std::invoke(std::forward<Function>(f),
+									  std::forward<Args>(args)...);
 		}
 		auto end = std::chrono::high_resolution_clock::now();
 		auto time = end - start;
-		return std::chrono::duration_cast<D>(time);
+		return std::chrono::duration_cast<Duration>(time);
 	}
 
 	template <class Function, class... Args>
 	requires std::invocable<Function, Args...> 
-		&& std::is_assignable_v<std::invoke_result_t<Function, Args...>&, std::invoke_result_t<Function, Args...>>
-	static
-	auto time_function(std::invoke_result_t<Function, Args...>& result, Function&& f, Args&&... args) -> D
+		  && std::is_assignable_v<std::invoke_result_t<Function, Args...>&,
+								  std::invoke_result_t<Function, Args...>>
+	[[nodiscard]] static auto time_function(
+		std::invoke_result_t<Function, Args...>& result, 
+		Function&& f, Args&&... args) -> Duration
 	{
 		auto start = std::chrono::high_resolution_clock::now();
 		result = std::invoke(std::forward<Function>(f), std::forward<Args>(args)...);
 		auto end = std::chrono::high_resolution_clock::now();
 		auto time = end - start;
-		return std::chrono::duration_cast<D>(time);
+		return std::chrono::duration_cast<Duration>(time);
 	}
 
 	template <class Function, class... Args>
 	requires std::invocable<Function, Args...>
-	static
-	auto time_function_repeatedly(std::size_t repeats, Function&& f, Args&&... args) -> Statistics<D>
+	[[nodiscard]] static auto time_function_repeatedly(
+		std::size_t repeats, Function&& f, Args&&... args) -> Statistics<Duration>
 	{
-		std::vector<D> results;
+		std::vector<Duration> results;
 		results.reserve(repeats);
-		//for (std::size_t i = 0; i < repeats; ++i)
-		//{
-		//	auto&& time = Timer::time_function(std::forward<Function>(f), std::forward<Args>(args)...);
-		//	results.emplace_back(time);
-		//}
-		std::generate_n(std::back_inserter(results), repeats, [&f, &args...] // works
-		//std::generate_n(std::back_inserter(results), repeats, [&f, ... args = std::forward<Args>(args)] // works
-		//std::generate_n(std::back_inserter(results), repeats, [f = std::forward<Function>(f), &args...] // compiler error if I also forward f in the body
-		//std::generate_n(std::back_inserter(results), repeats, [f = std::forward<Function>(f), ... args = std::forward<Args>(args)] // compiler error if I also forward f in the body
+		std::generate_n(std::back_inserter(results), repeats,
+			// Here are various ways in which i tried perfect forwarding in a lambda:
+			// works:
+			[&f, &args...]
+			// works:
+			//[&f, ... args = std::forward<Args>(args)]
+			// compiler error if I also forward f in the body:
+			//[f = std::forward<Function>(f), &args...]
+			// compiler error if I also forward f in the body:
+			//[f = std::forward<Function>(f), ... args = std::forward<Args>(args)]
 			{
-				return Timer::time_function(std::forward<Function>(f), std::forward<Args>(args)...);
+				return Timer::time_function(
+					std::forward<Function>(f), std::forward<Args>(args)...);
 			}
 		);
-		return Statistics<D>(std::move(results));
+		return Statistics<Duration>(std::move(results));
 	}
 
 	template <class OutputIt, class Function, class... Args>
 	requires std::invocable<Function, Args...>
-		&& std::output_iterator<OutputIt, std::invoke_result_t<Function, Args...>>
-	static
-	auto time_function_repeatedly(std::size_t repeats, OutputIt outputIt, Function&& f, Args&&... args) -> Statistics<D>
+		  && std::output_iterator<OutputIt, std::invoke_result_t<Function, Args...>>
+	[[nodiscard]] static auto time_function_repeatedly(
+		std::size_t repeats, OutputIt outputIt, Function&& f, Args&&... args) -> Statistics<Duration>
 	{
-		std::vector<D> results;
+		std::vector<Duration> results;
 		results.reserve(repeats);
-		std::generate_n(std::back_inserter(results), repeats, [&outputIt, &f, &args...] // works
-		//std::generate_n(std::back_inserter(results), repeats, [&outputIt, &f, ... args = std::forward<Args>(args)] // works
-		//std::generate_n(std::back_inserter(results), repeats, [&outputIt, f = std::forward<Function>(f), &args...] // compiler error if I also forward f in the body
-		//std::generate_n(std::back_inserter(results), repeats, [&outputIt, f = std::forward<Function>(f), ... args = std::forward<Args>(args)] // compiler error if I also forward f in the body
+		std::generate_n(std::back_inserter(results), repeats,
+			// Here are various ways in which i tried perfect forwarding in a lambda:
+			// works:
+			[&outputIt, &f, &args...]
+			// works:
+			//[&outputIt, &f, ... args = std::forward<Args>(args)]
+			// compiler error if I also forward f in the body:
+			//[&outputIt, f = std::forward<Function>(f), &args...]
+			// compiler error if I also forward f in the body:
+			//[&outputIt, f = std::forward<Function>(f), ... args = std::forward<Args>(args)]
 			{
-				return Timer::time_function(*outputIt++, std::forward<Function>(f), std::forward<Args>(args)...);
+				return Timer::time_function(
+					*outputIt++, std::forward<Function>(f), std::forward<Args>(args)...);
 			}
 		);
-		return Statistics<D>(std::move(results));
+		return Statistics<Duration>(std::move(results));
 	}
 };
 
